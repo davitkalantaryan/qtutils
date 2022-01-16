78873163658e23acd00abb1b00e118edc9d0338b
Davit Kalantaryan <davit.kalataryan@desy.de>
2022-01-16 12:41:05 +0100

    fixed some scripts

--- scripts/.cicd/unix_test_pull_to_master_build.sh
+++ scripts/.cicd/unix_test_pull_to_master_build.sh
@@ -41,7 +41,7 @@ elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 fi
 
 cd ${repositoryRoot}/prj/tests/googletest_mult
-unset CPPUTILS_DEBUG
+unset QTUTILS_DEBUG
 make -f unix.Makefile all
 # to make debug use line below
 make -f unix.Makefile all QTUTILS_DEBUG=1
--- scripts/.cicd/windows_test_pull_to_master_build.bat
+++ scripts/.cicd/windows_test_pull_to_master_build.bat
@@ -18,8 +18,8 @@ rem if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 nmake -f windows.Makefile Platform=!Platform! Configuration=!Configuration!
 if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
-rem cd "%repositoryRoot%workspaces\cpputils_vs"
-rem call msbuild "%repositoryRoot%workspaces\cpputils_vs\cpputils.sln" /t:%ActionConfirm% /p:Configuration=%Configuration% /p:Platform=%Platform% -m:2
+rem cd "%repositoryRoot%workspaces\qtutils_all_vs"
+rem call msbuild "%repositoryRoot%workspaces\qtutils_all_vs\qtutils_all.sln" /t:%ActionConfirm% /p:Configuration=%Configuration% /p:Platform=%Platform% -m:2
 rem if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
 rem do we need to compile makefile projects, then init env and do
