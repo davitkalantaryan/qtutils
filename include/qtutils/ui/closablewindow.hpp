a3eb98a25996e3149b48e2aa686f018d014e0251
Davit Kalantaryan <davit.kalataryan@desy.de>
2022-01-18 12:24:00 +0100

    added web style login type window

---
+++ include/qtutils/ui/closablewindow.hpp
@@ -0,0 +1,44 @@
+//
+// file:            webstylelogintypewindow.hpp
+// path:			include/qtutils/ui/webstylelogintypewindow.hpp
+// created on:		2022 Jan 18
+// created by:		Davit Kalantaryan (davit.kalantaryan@gmail.com)
+//
+
+#pragma once
+
+#ifndef QTUTILS_INCLUDE_WEBSTYLELOGINTYPEWINDOW_HPP
+#define QTUTILS_INCLUDE_WEBSTYLELOGINTYPEWINDOW_HPP
+
+
+#include <qtutils/qtutils_internal_header.h>
+#include <qtutils/ui/resizible_window.hpp>
+#include <qtutils/disable_utils_warnings.h>
+#include <QWidget>
+
+
+namespace qtutils { namespace ui{
+
+template <typename WidgetType>
+class WebStyleLoginTypeWindow : public ::qtutils::ui::ResizibleWindow<QWidget>{
+public:
+    template<typename... Targs>
+	WebStyleLoginTypeWindow(Targs... a_args);
+    virtual ~WebStyleLoginTypeWindow() override;
+	
+protected:
+	void resizeEvent(QResizeEvent *event) override;
+	
+protected:
+	WidgetType		m_ltWnd;
+};
+
+}}  // namespace qtutils { namespace ui{
+
+
+#ifndef QTUTILS_INCLUDE_WEBSTYLELOGINTYPEWINDOW_IMPL_HPP
+#include "webstylelogintypewindow.impl.hpp"
+#endif
+
+
+#endif  // #ifndef QTUTILS_INCLUDE_WEBSTYLELOGINTYPEWINDOW_HPP
---
+++ include/qtutils/ui/logintypewindow.hpp
@@ -0,0 +1,42 @@
+//
+// file:            logintypewindow.hpp
+// path:			include/qtutils/ui/logintypewindow.hpp
+// created on:		2022 Jan 18
+// created by:		Davit Kalantaryan (davit.kalantaryan@gmail.com)
+//
+
+#pragma once
+
+#ifndef QTUTILS_INCLUDE_LOGINTYPEWINDOW_HPP
+#define QTUTILS_INCLUDE_LOGINTYPEWINDOW_HPP
+
+
+#include <qtutils/qtutils_internal_header.h>
+#include <qtutils/ui/resizible_window.hpp>
+#include <qtutils/disable_utils_warnings.h>
+#if defined(CPPUTILS_EMSCRIPTEN_IS_USED) || defined(QTUTILS_UI_FORCE_WEB_STYLE)
+#include <qtutils/ui/webstylelogintypewindow.hpp>
+#else
+#include <qtutils/ui/resizible_window.hpp>
+#endif
+
+
+namespace qtutils { namespace ui{
+
+#if defined(CPPUTILS_EMSCRIPTEN_IS_USED) || defined(QTUTILS_UI_FORCE_WEB_STYLE)
+
+template <typename WidgetType >
+using LoginTypeWindow = WebStyleLoginTypeWindow<WidgetType>;
+
+#else
+
+template <typename WidgetType >
+using LoginTypeWindow = ResizibleWindow<WidgetType>;
+
+#endif
+
+
+}}  // namespace qtutils { namespace ui{
+
+
+#endif  // #ifndef QTUTILS_INCLUDE_LOGINTYPEWINDOW_HPP
