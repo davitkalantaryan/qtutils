6704c34f03f285710e03ee6273dff61638421426
Davit Kalantaryan <davit.kalataryan@desy.de>
2022-01-24 02:58:55 +0100

    some definations for lweb login type widgets

--- include/qtutils/ui/logintypewindow.hpp
+++ include/qtutils/ui/logintypewindow.hpp
@@ -24,15 +24,15 @@ namespace qtutils { namespace ui{
 
 #if defined(CPPUTILS_EMSCRIPTEN_IS_USED) || defined(QTUTILS_UI_FORCE_WEB_STYLE)
 
-//#define QTUTILS_HANDLE_WIDGETP()        parentWidget()
-//#define QTUTILS_HANDLE_WIDGETC(_ww)     ((_ww)->childWdg())
+#define QTUTILS_LOGIN_WND_PARENT(_lw)   ((_lw)->parentWidget())
+#define QTUTILS_LOGIN_WND_WIDGETC(_lw)  ((_lw)->childWdg())
 template <typename WidgetType >
 using LoginTypeWindow = WebStyleLoginTypeWindow<WidgetType>;
 
 #else
 
-//#define QTUTILS_HANDLE_WIDGETP()        ( this )
-//#define QTUTILS_HANDLE_WIDGETC(_ww)     (_ww)
+#define QTUTILS_LOGIN_WND_PARENT(_lw)     (_lw)
+#define QTUTILS_LOGIN_WND_WIDGETC(_lw)    (_lw)
 template <typename WidgetType >
 class LoginTypeWindow : public ResizibleWindowRaw<WidgetType>
 {
