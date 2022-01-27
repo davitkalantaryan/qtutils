7b299cc5e4f72da4ed5d00421d028e8c59861242
Davit Kalantaryan <davit.kalataryan@desy.de>
2022-01-27 12:39:26 +0100

    Added hash with QString key

---
+++ include/qtutils/core/hashtbl.hpp
@@ -0,0 +1,30 @@
+//
+// file:            hashtbl.hpp
+// path:			include/qtutils/core/hashtbl.hpp
+// created on:		2022 Jan 27
+// created by:		Davit Kalantaryan (davit.kalantaryan@gmail.com)
+//
+
+#pragma once
+
+
+#include <qtutils/qtutils_internal_header.h>
+#include <cpputils/hashtbl.hpp>
+#include <qtutils/disable_utils_warnings.h>
+#include <QString>
+
+
+namespace qtutils {  namespace hashtbl { 
+
+
+class HashByQString 
+{
+public:
+	size_t	operator()(const QString& key)const;
+};
+
+template <typename DataType,typename Hash=HashByQString,size_t templateDefaultSize=DEFAULT_TABLE_SIZE >
+using QStrHash = ::cpputils::hashtbl::Base< QString,DataType,Hash,templateDefaultSize >;
+
+
+}}  // namespace qtutils {  namespace hashtbl {
