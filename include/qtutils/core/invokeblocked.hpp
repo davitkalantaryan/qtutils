1193fb4310489ea77125bfdcec48bc4a5c0c45c0
DAVIT Kalantaryan <Davit Kalantaryan>
2022-05-01 00:05:55 +0200

    added method to call api on behalf of any thread and make sure that the call is always blocked

---
+++ include/qtutils/core/invokeblocked.hpp
@@ -0,0 +1,33 @@
+//
+// file:            invokeblocked.hpp
+// path:			include/qtutils/core/invokeblocked.hpp
+// created on:		2022 Apr 30
+// created by:		Davit Kalantaryan (davit.kalantaryan@gmail.com)
+//
+
+
+#ifndef QTUTILS_INCLUDE_QTUTILS_CORE_INVOKEBLOCKED_HPP
+#define QTUTILS_INCLUDE_QTUTILS_CORE_INVOKEBLOCKED_HPP
+
+#include <qtutils/qtutils_internal_header.h>
+#include <functional>
+#include <qtutils/disable_utils_warnings.h>
+#include <QObject>
+
+namespace qtutils {
+
+
+template<typename... Targs>
+bool invokeMethodBlocked(QObject* a_pObj, const ::std::function<void(Targs...)>& a_fnc, Targs... a_args);
+QTUTILS_EXPORT bool invokeMethodBlocked(QObject* a_pObj, const ::std::function<void(void)>& a_fnc);
+
+
+}  // namespace qtutils {
+
+
+#ifndef QTUTILS_INCLUDE_QTUTILS_CORE_INVOKEBLOCKED_IMPL_HPP
+#include "invokeblocked.impl.hpp"
+#endif
+
+
+#endif  // #ifndef QTUTILS_INCLUDE_QTUTILS_CORE_INVOKEBLOCKED_HPP
