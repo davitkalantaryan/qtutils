ba1264420d3f7d597eaa4dc86c3cd4f9e6b5eee1
DAVIT Kalantaryan <Davit Kalantaryan>
2023-05-12 23:00:36 +0200

    problem of not getting back from minimized state resolved

--- include/qtutils/ui/resiziblewindow.impl.hpp
+++ include/qtutils/ui/resiziblewindow.impl.hpp
@@ -84,7 +84,9 @@ void ResizibleWindowRaw<WidgetType>::InitAndShow()
 template <typename WidgetType>
 inline bool ResizibleWindowRaw<WidgetType>::InitAndShowBase()
 {
+    bool bIsMinimized = false;
     if(m_flags.b.initNotCalled){
+        bIsMinimized = true;
         m_flags.b2.initCalledOrNot = CPPUTILS_MAKE_BITS_TRUE;
         m_settingsKey = typeid(*this).name()+QString::number(m_flags.b.instanceNumber);
         this->InitRaw();
@@ -92,7 +94,6 @@ inline bool ResizibleWindowRaw<WidgetType>::InitAndShowBase()
 
     Settings aSettings;
     bool bIsMaximized = false;
-    bool bIsMinimized = false;
 
     if(aSettings.contains(m_settingsKey+QTUTILS_RSBL_WND_POS_KEY)){
         const QPoint aPos = aSettings.value(m_settingsKey+QTUTILS_RSBL_WND_POS_KEY).toPoint();
@@ -103,18 +104,23 @@ inline bool ResizibleWindowRaw<WidgetType>::InitAndShowBase()
         bIsMaximized = aSettings.value(m_settingsKey+QTUTILS_RSBL_WND_IS_MAXIMIZED_KEY).toBool();
     }
 
-    if(aSettings.contains(m_settingsKey+QTUTILS_RSBL_WND_IS_MINIMIZED_KEY)){
-        bIsMinimized = aSettings.value(m_settingsKey+QTUTILS_RSBL_WND_IS_MINIMIZED_KEY).toBool();
+    if(bIsMinimized){
+        if(aSettings.contains(m_settingsKey+QTUTILS_RSBL_WND_IS_MINIMIZED_KEY)){
+            bIsMinimized = aSettings.value(m_settingsKey+QTUTILS_RSBL_WND_IS_MINIMIZED_KEY).toBool();
+        }
     }
-
 
     if(bIsMaximized){
-        WidgetType::showMaximized();
-        return false;
+        if(!(WidgetType::isMaximized())){
+            WidgetType::showMaximized();
+            return false;
+        }
     }
     else if(bIsMinimized){
-        WidgetType::showMinimized();
-        return false;
+        if(!(WidgetType::isMinimized())){
+            WidgetType::showMinimized();
+            return false;
+        }
     }
     return true;
 }
