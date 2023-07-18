dbeb34828d1394deeee50afa518f697355db39f2
DAVIT Kalantaryan <Davit Kalantaryan>
2023-07-18 20:48:03 +0200

    implemented new type of widgts container that can have several widgets

---
+++ include/qtutils/ui/widgetscontainerlogin.hpp
@@ -0,0 +1,42 @@
+//
+// repo:            qtutils
+// file:            widgetscontainerlogin.hpp
+// path:			include/qtutils/ui/widgetscontainerlogin.hpp
+// created on:		2023 Jul 02
+// created by:		Davit Kalantaryan (davit@focust.ai)
+//
+
+#pragma once
+
+#include <qtutils/export_symbols.h>
+#include <qtutils/ui/widgetscontainerbasequ.hpp>
+
+
+namespace qtutils { namespace ui{
+
+#define QUTILS_WIDG_CONT_LOGIN_LOGIN    0
+#define QUTILS_WIDG_CONT_LOGIN_MAINW    1
+
+
+class QTUTILS_UI_EXPORT WidgetsContainerLogin : public WidgetsContainerBase
+{
+
+public:
+    virtual ~WidgetsContainerLogin() override;
+    WidgetsContainerLogin(QWidget* CPPUTILS_ARG_NN a_pLoginWnd, QWidget* CPPUTILS_ARG_NN a_pMainWindow,
+                          const QString& a_logo, const QString& a_tooltip,const QString& a_exitLogo,
+                          const TypeWCShowHide& a_login_show, const TypeWCShowHide& a_main_show);
+    virtual void show() override;
+    void MakeLoginCloseToExitApp();
+    void MakeLoginCloseToHideLogin();
+
+public slots:
+    void SetVisible();
+    void SetHidden();
+    
+protected:
+    void ConnectSignalsQuLogin();
+};
+
+
+}}  //  namespace qtutils { namespace ui{
