eb926bb4ca0bc32779250075053ba0a9389bde05
Davit Kalantaryan <davit.kalantar@desy.de>
2023-08-23 13:28:52 +0200

    fixed scripts/windows_build_all.bat file

--- scripts/windows_build_all.bat
+++ scripts/windows_build_all.bat
@@ -52,7 +52,7 @@ for %%x in (%*) do (
 )
 
 echo action=%ActionConfirm%,PlatformTarget=!PlatformTarget!,configuration=%Configuration%
-cd "%repositoryRoot%prj\tests\cpputils_unit_test_mult"
+cd "%repositoryRoot%prj\tests\qtutils_unit_test_mult"
 if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
 for %%p in (%PlatformTarget%) do (
@@ -62,7 +62,7 @@ for %%p in (%PlatformTarget%) do (
 		rem call msbuild "%repositoryRoot%workspaces\cpputils_all_vs\cpputils_all.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
 		call msbuild "%repositoryRoot%workspaces\qtutils_all_vs\qtutils_all.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
 		if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
-		call nmake -f cpputils_unit_test.windows.Makefile /e Platform=%%p /e Configuration=%%c
+		call nmake -f qtutils_unit_test.windows.Makefile /e Platform=%%p /e Configuration=%%c
 		if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 	)
 )
