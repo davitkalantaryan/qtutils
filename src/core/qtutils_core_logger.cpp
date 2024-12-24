b7c67a25a68fb86808abc50775a4b23de75f9dd1
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-12-24 15:57:47 +0100

    some improvemenets

--- src/core/qtutils_core_logger.cpp
+++ src/core/qtutils_core_logger.cpp
@@ -9,242 +9,197 @@
 #define cinternal_lw_recursive_mutex_create_needed
 #include <cinternal/lw_mutex_recursive.h>
 #include <cinternal/disable_compiler_warnings.h>
-#include <mutex>
+#include <new>
 #include <cinternal/undisable_compiler_warnings.h>
 #include <qtutils/disable_utils_warnings.h>
 #include <QFileInfo>
 #include <QDateTime>
 
 
-namespace qtutils{
+namespace qtutils { namespace logger{
 
 
-class CPPUTILS_DLL_PRIVATE mutex_ml final{
+class CPPUTILS_DLL_PRIVATE DefaultLogDeleter final
+{
 public:
-	mutex_ml();
-	~mutex_ml();
-	void lock();
-	void unlock();
-	
-private:
-	cinternal_lw_recursive_mutex_t m_mutex;
+    ~DefaultLogDeleter();
 };
 
-
-static QtMessageHandler s_defaultHandler    = nullptr;
-static Logger_p* s_pFirstLogger             = nullptr;
-static mutex_ml s_logsMutex;
+static Base*            s_pDefaultLogger = nullptr;
+static QtMessageHandler s_defaultHandler = nullptr;
 
 static void MessageHandlerStatic(QtMsgType a_type, const QMessageLogContext& a_context,const QString& a_message);
 
-static void MessageHandlerStaticForNull(void*,QtMsgType, const QMessageLogContext &,const QString &)
-{
+
+static void DeleteDefaultLogInline(void) noexcept {
+    if(s_pDefaultLogger){
+        delete s_pDefaultLogger;
+        s_pDefaultLogger = nullptr;
+    }
 }
 
-static void MessageHandlerStaticForDefault(void* a_def,QtMsgType a_msgType, const QMessageLogContext& a_ctx,const QString& a_msg)
-{
-    const QtMessageHandler defhand = (QtMessageHandler)a_def;
-    defhand(a_msgType,a_ctx,a_msg);
+
+static void AddDefaultLoggerInline(void){
+    if(!s_pDefaultLogger){
+        s_pDefaultLogger = new Default();
+    }
 }
 
 
-/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+static inline CinternalLogCategory QtCategoryToCinternalInline(const QtMsgType& a_cat) noexcept {
+    switch (a_cat) {
+    case QtDebugMsg:
+        return CinternalLogCategoryDebug;
+    case QtWarningMsg:
+        return CinternalLogCategoryWarning;
+    case QtCriticalMsg:
+        return CinternalLogCategoryCritical;
+    case QtInfoMsg:
+        return CinternalLogCategoryInfo;
+    default:  // remains QtSystemMsg and QtCriticalMsg
+        break;
+    }  //  switch (a_type) {
+    return CinternalLogCategoryFatal;
+}
 
-class CPPUTILS_DLL_PRIVATE Logger_p final
-{
-public:
-    Logger_p(const Logger::TypeLogger& a_logger, void* a_pOwner);
-    ~Logger_p();
-    void MessageHandler(QtMsgType a_type, const QMessageLogContext& a_context,const QString& a_message);
 
-private:
-    Logger_p*           m_prev;
+static inline QtMsgType CinternalLogCategoryQtLogTypeInline(const CinternalLogCategory& a_logCat){
+    switch(a_logCat){
+    case CinternalLogCategoryFatal:
+        return QtFatalMsg;
+    case CinternalLogCategoryCritical:
+        return QtCriticalMsg;
+    case CinternalLogCategoryWarning:
+        return QtWarningMsg;
+    case CinternalLogCategoryInfo:
+        return QtInfoMsg;
+    default:
+        break;
+    }  //  switch(a_logCat){
+    return QtDebugMsg;
+}
 
-public:
-    Logger_p*           m_next;
-    void*               m_pOwner;
-    Logger::TypeLogger  m_logger;
-};
+
+static inline void InitializeQtLogger(){
+    if(!s_defaultHandler){
+        AddDefaultLoggerInline();
+        s_defaultHandler = qInstallMessageHandler(&MessageHandlerStatic);
+        CinternalLoggerRemoveDefaultlyAddedLogger();
+    }
+}
 
 
 /*////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-Logger::Logger(const TypeLogger& a_logger, void* a_pOwner)
+
+Base::~Base() noexcept
+{
+    CinternalLoggerRemoveLogger(m_loggerItem);
+}
+
+
+Base::Base(const char* a_cpcEndStr)
     :
-      m_logger_data_p(new Logger_p(a_logger?a_logger:&MessageHandlerStaticForNull,a_pOwner))
+    m_loggerItem(CinternalLoggerAddLogger(&Base::LoggerClbkSt,this,a_cpcEndStr))
 {
-}
-
-
-Logger::~Logger()
-{
-    delete m_logger_data_p;
-}
-
-
-void Logger::SetNewLogger(const TypeLogger& a_logger, void* a_pOwner)
-{
-    ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-    m_logger_data_p->m_logger = a_logger?a_logger:&MessageHandlerStaticForNull;
-    m_logger_data_p->m_pOwner = a_pOwner;
-}
-
-
-void Logger::SetLoggerToDefault()
-{
-    ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-
-    if(s_defaultHandler){
-        m_logger_data_p->m_logger = &MessageHandlerStaticForDefault;
-        m_logger_data_p->m_pOwner = (void*)s_defaultHandler;
+    InitializeQtLogger();
+    if(!m_loggerItem){
+        throw ::std::bad_alloc();
     }
 }
 
 
-QtMessageHandler Logger::DefaultHandler()
+Base::Base(const Base& a_cM)
+    :
+    m_loggerItem(CinternalLoggerAddLogger(&Base::LoggerClbkSt,this,CinternalLoggerGetEndString(a_cM.m_loggerItem)))
 {
-    QtMessageHandler retHandler = nullptr;
-
-    {  //  start lock
-        ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-
-        if(s_defaultHandler){
-            retHandler = s_defaultHandler;
-        }
-        else{
-            retHandler = s_defaultHandler = qInstallMessageHandler(&MessageHandlerStatic);
-            qInstallMessageHandler(s_defaultHandler);
-        }
-
-    }  //  end locks
-
-    return retHandler;
+    if(!m_loggerItem){
+        throw ::std::bad_alloc();
+    }
 }
 
 
-QString Logger::FileAndLineString(const char* a_fileName, int a_line)
+Base::Base(Base&& a_mM) noexcept
+    :
+    m_loggerItem(a_mM.m_loggerItem)
 {
-    if(a_line>0){
-        return QString("fl:") + QFileInfo(a_fileName).fileName() + QString(",ln:") + QString::number(a_line);
-    }
-    return "";
+    a_mM.m_loggerItem = nullptr;
 }
 
 
-QString Logger::FileLineAndFunctionString(const char* a_fileName, int a_line, const char* a_cpcFunction)
+Base& Base::operator=(const Base& a_cM)
 {
-    if(a_line>0){
-        return FileAndLineString(a_fileName,a_line) + QString(",fn:\"") + a_cpcFunction+ "\": ";
+    const char* cpcNewEpcndStr = CinternalLoggerGetEndString(a_cM.m_loggerItem);
+    const int nRet = CinternalLoggerSetEndString(m_loggerItem,cpcNewEpcndStr);
+    if(nRet){
+        throw ::std::bad_alloc();
     }
-    return "";
+    return *this;
+}
+
+
+Base& Base::operator=(Base&& a_mM) noexcept
+{
+    const CinternalLoggerItem* const loggerItem = m_loggerItem;
+    m_loggerItem = a_mM.m_loggerItem;
+    a_mM.m_loggerItem = loggerItem;
+    return *this;
+}
+
+
+void Base::LoggerClbkSt(void* a_userData, enum CinternalLogCategory a_categoryEnm, const char* CPPUTILS_ARG_NN a_categoryStr, char* CPPUTILS_ARG_NN a_log, size_t a_logStrLen)
+{
+    Base* const thisPtr = static_cast<Base*>(a_userData);
+    thisPtr->LoggerClbk(a_categoryEnm,a_categoryStr,a_log,a_logStrLen);
 }
 
 
 /*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-void Logger_p::MessageHandler(QtMsgType a_type, const QMessageLogContext& a_context,const QString& a_message)
+void Default::LoggerClbk(CinternalLogCategory a_categoryEnm, const char* CPPUTILS_ARG_NN a_categoryStr, char* CPPUTILS_ARG_NN a_log, size_t a_logStrLen)
 {
-    QString msgCntx;
-    if(a_context.line>0){
-        msgCntx = QDateTime::currentDateTime().toString() + ": ";
-    
-        switch (a_type) {
-        case QtDebugMsg:
-            msgCntx += "debug:    ";
-            break;
-        case QtWarningMsg:
-            msgCntx += "warning:  ";
-            msgCntx += Logger::FileAndLineString(a_context.file,a_context.line) + ": ";
-            break;
-        case QtFatalMsg:
-            msgCntx += "fatal:    ";
-            msgCntx += Logger::FileLineAndFunctionString(a_context.file,a_context.line,a_context.function);
-            break;
-        case QtInfoMsg:
-            msgCntx += "info:     ";
-            break;
-        default:  // remains QtSystemMsg and QtCriticalMsg
-            msgCntx += "critical: ";
-            msgCntx += Logger::FileLineAndFunctionString(a_context.file,a_context.line,a_context.function);
-            break;
-        }  //  switch (a_type) {
-    }  //  if(a_context.line>0){
+    // QtMsgType a_type, const QMessageLogContext& a_context,const QString& a_message
+    const QtMsgType qtLogType = CinternalLogCategoryQtLogTypeInline(a_categoryEnm);
+    const QMessageLogContext aCtx({2,-1,"","",a_categoryStr});
+    const QString aMessage = QString(a_log);
 
-    m_logger(m_pOwner,a_type,a_context,msgCntx+a_message);
+    static_cast<void>(a_logStrLen);
+    (*s_defaultHandler)(qtLogType,aCtx,aMessage);
 }
 
 
 /*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-
-Logger_p::Logger_p(const Logger::TypeLogger& a_logger, void* a_pOwner)
-    :
-      m_prev(nullptr),
-      m_pOwner(a_pOwner),
-      m_logger(a_logger)
-{
-    ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-
-    if(s_pFirstLogger){
-        s_pFirstLogger->m_prev = this;
-    }
-    else{
-        if(!s_defaultHandler){
-            s_defaultHandler = qInstallMessageHandler(&MessageHandlerStatic);
-        }
-    }
-
-    m_next = s_pFirstLogger;
-    s_pFirstLogger = this;
-}
-
-
-Logger_p::~Logger_p()
-{
-    ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-
-    if(m_prev){m_prev->m_next = m_next;}
-    if(m_next){m_next->m_prev = m_prev;}
-    if(this==s_pFirstLogger){
-        qInstallMessageHandler(s_defaultHandler);
-        s_defaultHandler = nullptr;
-        s_pFirstLogger = nullptr;
-    }
-}
-
 
 static void MessageHandlerStatic(QtMsgType a_type, const QMessageLogContext& a_context,const QString& a_message)
 {
-    Logger_p* pLogger;
-    ::std::lock_guard<mutex_ml> aGuard(s_logsMutex);
-    pLogger = s_pFirstLogger;
-    while(pLogger){
-        pLogger->MessageHandler(a_type,a_context,a_message);
-        pLogger = pLogger->m_next;
-    }
+    const CinternalLogCategory logType = QtCategoryToCinternalInline(a_type);
+    const ::std::string logMsg = a_message.toStdString();
+    const char* const pcLogMsg = logMsg.c_str();
+    CinternalLoggerMakeLog(0,a_context.category,a_context.file,a_context.line,a_context.file,CinternalLogTypeCompleteLoggingWithPlaceAndFunc,logType,"%s",pcLogMsg);
 }
 
 
 /*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-mutex_ml::mutex_ml()
+DefaultLogDeleter::~DefaultLogDeleter()
 {
-	cinternal_lw_recursive_mutex_create(&m_mutex);
-}
-
-mutex_ml::~mutex_ml()
-{
-	cinternal_lw_recursive_mutex_destroy(&m_mutex);
-}
-
-void mutex_ml::lock()
-{
-	cinternal_lw_recursive_mutex_lock(&m_mutex);
-}
-
-void mutex_ml::unlock()
-{
-	cinternal_lw_recursive_mutex_unlock(&m_mutex);
+    DeleteDefaultLogInline();
 }
 
 
-}  // namespace qtutils{
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+QTUTILS_EXPORT Base* GetDefaultlyAddedLogger(void) noexcept
+{
+    return s_pDefaultLogger;
+}
+
+
+QTUTILS_EXPORT void RemoveDefaultlyAddedLogger(void) noexcept
+{
+    DeleteDefaultLogInline();
+}
+
+
+
+}}  //  namespace qtutils { namespace logger{
