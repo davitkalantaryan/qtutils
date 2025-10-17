3c1b9776a60c1db14ba4993af8e0cf21f3d40ddf
Davit Kalantaryan <davit.kalantaryan@desy.de>
2025-10-17 12:44:58 +0200

    fixed warning name in the warning disable header

--- include/qtutils/disable_utils_warnings.h
+++ include/qtutils/disable_utils_warnings.h
@@ -38,7 +38,7 @@
 #endif
 
 #if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic ignored "-Warray-bounds="
+#pragma GCC diagnostic ignored "-Warray-bounds"
 #endif
 
 #ifdef CINTERNAL_INCLUDE_CINTERNAL_DISABLE_COMPILER_WARNINGS_H_SHOULD_BE_DEFINED
