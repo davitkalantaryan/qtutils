d6143d18ee50bd6ab0e4dfa74e6b2ac3e341463d
Davit Kalantaryan <davit.kalantaryan@desy.de>
2026-09-04 18:22:38 +0200

    ThreadLS improved. Some functions declared as noexcept and class can return native handle

--- include/qtutils/core/threadls.hpp
+++ include/qtutils/core/threadls.hpp
@@ -11,45 +11,57 @@
 
 #ifndef QTUTILS_NOT_USE_THREADLS
 
+#include <cinternal/disable_compiler_warnings.h>
 #include <functional>
+#include <thread>
 #include <qtutils/disable_utils_warnings.h>
 #include <QThread>
+#include <cinternal/undisable_compiler_warnings.h>
 
 // LS stands for Late Start, constructor exits when thread stated
 
-namespace qtutils{
+namespace qtutils{ namespace core{
 
 class CPPUTILS_DLL_PRIVATE ThreadLS_p;
 
 class QTUTILS_EXPORT ThreadLS final
 {
 public:
-    //typedef void (*TypeConstruct)(void*);
-    //typedef void (*TypeDestruct)(void*);
 	typedef ::std::function<void(void*)>	TypeConstruct;
 	typedef ::std::function<void(void*)>	TypeDestruct;
     typedef ::std::function<void(void*)>	TypeMain;
 	
 public:
+    ~ThreadLS();
     ThreadLS();
     ThreadLS(const TypeConstruct& a_construct, const TypeDestruct& a_destruct, void* a_data);
     ThreadLS(const TypeMain& a_main, const TypeConstruct& a_construct, const TypeDestruct& a_destruct, void* a_data);
-    ~ThreadLS();
-    ThreadLS(const ThreadLS&)=delete;
-    ThreadLS(ThreadLS&&);
-    ThreadLS& operator=(const ThreadLS&)=delete;
-    ThreadLS& operator=(ThreadLS&&);
+    ThreadLS(ThreadLS&&) noexcept;
+    ThreadLS& operator=(ThreadLS&&) noexcept;
 
-    QThread* qThread()const;
-    void EnableExceptionsHandling();
-    void DisableExceptionsHandling();
-    bool hasExceptionHandling()const;
+    QThread* qThread() noexcept;
+    const QThread* qThread()const noexcept;
+    void EnableExceptionsHandling() noexcept;
+    void DisableExceptionsHandling() noexcept;
+    bool hasExceptionHandling()const noexcept;
+    ::std::thread::native_handle_type getNativeHandle()const noexcept;
+    ::std::thread::native_handle_type GetAndResetNativeHandle() noexcept;
 
 private:
     ThreadLS_p*        m_thr_data_p;
+
+private:
+    ThreadLS(const ThreadLS&)=delete;
+    ThreadLS& operator=(const ThreadLS&)=delete;
 };
 
 
-}  // namespace qtutils{
+}  //  namespace core
+
+
+typedef core::ThreadLS ThreadLS;
+
+
+}  //  namespace qtutils{
 
 #endif  // #ifndef QTUTILS_NOT_USE_THREADLS
